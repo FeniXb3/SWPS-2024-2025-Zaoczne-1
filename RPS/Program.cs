1fa994e0e823b87c9444cc3be5e38bb0d7dfef10
Konrad Gadzina <konrad@enklawa.edu.pl>
2024-12-22 12:49:32 +0100

    Add points

--- RPS/Program.cs
+++ RPS/Program.cs
@@ -1,28 +1,40 @@
-﻿                        // 0        1        2
+﻿//                         0        1        2
 string[] allowedSigns = ["fire", "water", "grass"];
+int expectedPoints = 3;
+int firstPlayerPoints = 0;
+int secondPlayerPoints = 0;
 
 Console.WriteLine($"Zagrajmy w {string.Join(" ", allowedSigns)}");
 
-string firstSign = GetSign(1);
-string secondSign = GetSign(2);
-
-// if (instrukcja któej wynikiem będzie true lub false)
-if (firstSign == secondSign)
+while (firstPlayerPoints < expectedPoints && secondPlayerPoints < expectedPoints)
 {
-    Console.WriteLine("Remis");
-}
-else if ((firstSign == allowedSigns[0] && secondSign == allowedSigns[2]) 
-        || (firstSign == allowedSigns[1] && secondSign == allowedSigns[0]) 
-        || (firstSign == allowedSigns[2] && secondSign == allowedSigns[1]))
-{
-    Console.WriteLine("G1 MISZCZ!");
-}
-else
-{
-    Console.WriteLine("G2 MISZCZ!");
-}
+    string firstSign = GetSign(1);
+    string secondSign = GetSign(2);
 
+    // if (instrukcja któej wynikiem będzie true lub false)
+    if (firstSign == secondSign)
+    {
+        Console.WriteLine("Remis");
+    }
+    else if ((firstSign == allowedSigns[0] && secondSign == allowedSigns[2])
+            || (firstSign == allowedSigns[1] && secondSign == allowedSigns[0])
+            || (firstSign == allowedSigns[2] && secondSign == allowedSigns[1]))
+    {
+        Console.WriteLine("G1 MISZCZ!");
+        // poniższe trzy linijki robią to samo
+        // to,z której skorzystasz zależy od konktekstu i tego,co chcesz osiągnąć
+        // firstPlayerPoints = firstPlayerPoints - 1;
+        firstPlayerPoints += 1;
+        // firstPlayerPoints++;
+    }
+    else
+    {
+        Console.WriteLine("G2 MISZCZ!");
+        secondPlayerPoints += 1;
+    }
 
+    Console.WriteLine($"G1: {firstPlayerPoints} - G2: {secondPlayerPoints}");
+}
 Console.WriteLine("GG");
 
 
