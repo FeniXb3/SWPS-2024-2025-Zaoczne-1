23fe5becdd5261d349e5817d546d6dc02ce904bb
Konrad Gadzina <konrad@enklawa.edu.pl>
2024-12-22 08:22:10 +0100

    Add RPS

---
+++ RPS/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Zagrajmy w rock paper scissors");
+Console.WriteLine("Graczu 1, podaj znak");
+// typ nazwa;
+// typ nazwa = wartosc;
+string? firstSign = Console.ReadLine();
+
+while (firstSign != "rock" && firstSign != "paper" && firstSign != "scissors")
+{
+    Console.WriteLine("Niepoprawny znak!");
+    Console.WriteLine("Graczu 1, podaj POPRAWNY znak");
+    firstSign = Console.ReadLine();
+}
+
+Console.WriteLine("Graczu 2, podaj znak");
+string? secondSign = Console.ReadLine();
+
+// if (instrukcja któej wynikiem będzie true lub false)
+if (firstSign == secondSign)
+{
+    Console.WriteLine("Remis");
+}
+else if ((firstSign == "rock" && secondSign == "scissors") 
+        || (firstSign == "paper" && secondSign == "rock") 
+        || (firstSign == "scissors" && secondSign == "paper"))
+{
+    Console.WriteLine("G1 MISZCZ!");
+}
+else
+{
+    Console.WriteLine("G2 MISZCZ!");
+}
+
+
+Console.WriteLine("GG");
